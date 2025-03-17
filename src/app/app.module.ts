76a55f4d910e1ea1d12c75e3683308bf3c3d8fb0
vinodh1988 <k.c.vinodh@hotmail.com>
2025-03-17 17:51:42 +0530

    child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent //components,directives and pipes
+    AppComponent,
+    InfoComponent //components,directives and pipes
   ],
   imports: [
     BrowserModule,
